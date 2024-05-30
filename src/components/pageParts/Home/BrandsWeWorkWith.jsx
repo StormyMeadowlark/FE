5f1e6a3390524cebfe7e7dfa2b08d79834a2db14
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-05-29 22:23:16 -0500

    Just needs correct images and links

--- src/components/pageParts/Home/BrandsWeWorkWith.jsx
+++ src/components/pageParts/Home/BrandsWeWorkWith.jsx
@@ -8,28 +8,28 @@ function BrandsWeWorkWith() {
 <div className="grid place-items-center bg-[#00ff00] h-96"> 
 <div className="gallery">
   <span style={{"--i":1}}>
-    <img className="brightness-50" src="https://images4.alphacoders.com/678/thumb-1920-678317.jpg" alt="" />
+    <img className="" src="src/assets/images/Affordability.jpg" alt="" />
   </span>
   <span style={{"--i":2}}>
-    <img  className="brightness-50" src="https://images5.alphacoders.com/653/653698.jpg" alt="" />
+    <img  className="" src="src/assets/images/Alignment.jpg" alt="" />
   </span>
   <span style={{"--i":3}}>
-    <img  className="brightness-50" src="https://images6.alphacoders.com/803/thumb-1920-803643.png" alt="" />
+    <img  className="" src="src/assets/images/Alignment2.jpg"  alt="" />
   </span>
   <span style={{"--i":4}}>
-    <img  className="brightness-50" src="https://images.alphacoders.com/785/thumb-1920-785833.png" alt="" />
+    <img  className="" src="src/assets/images/AllServices.jpeg" alt="" />
   </span>
   <span style={{"--i":5}}>
-    <img  className="brightness-50" src="https://images6.alphacoders.com/749/thumb-1920-749966.png" alt="" />
+    <img  className="" src="src/assets/images/Brakes.jpg"  alt="" />
   </span>
   <span style={{"--i":6}}>
-    <img  className="brightness-50" src="https://images4.alphacoders.com/761/thumb-1920-761076.png" alt="" />
+    <img  className="" src="src/assets/images/Brakes2.JPG"  alt="" />
   </span>
   <span style={{"--i":7}}>
-    <img  className="brightness-50" src="https://images.alphacoders.com/682/thumb-1920-682570.png" alt="" />
+    <img  className="" src="src/assets/images/Brake3.jpg" alt="" />
   </span>
   <span style={{"--i":8}}>
-    <img  className="brightness-50" src="https://images4.alphacoders.com/866/thumb-1920-866812.png" alt="" />
+    <img  className="" src="src/assets/images/CAR.png"  alt="" />
   </span>
 </div>
 </div>
