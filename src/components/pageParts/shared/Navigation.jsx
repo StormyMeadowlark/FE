797c9a0bbf09c5f149293165ae0bcb03381d1c6c
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-06-01 13:36:42 -0500

    svg is viewbox is the correct size.

--- src/components/pageParts/shared/Navigation.jsx
+++ src/components/pageParts/shared/Navigation.jsx
@@ -1,5 +1,6 @@
 import '../../../App.css'
-
+import useHover from '../../../hooks/useHover'
+import GearWrenchIcon from '../../GearAndWrenchFromLogo.svg?react'
 
 const navigation = [
     { name: 'SERVICE', href: '#' },
@@ -8,16 +9,42 @@ const navigation = [
     { name: 'CONTACT', href: '#' },
     { name: 'AUTO SCHOOL', href: '#' },
   ]
-
-
+const service = { name: 'SERVICE', href: '#', }
+const factAnswerQuestion = { name: 'FAQ', href: '#', } 
+const about = { name: 'ABOUT', href: '#', } 
+const contact = { name: 'CONTACT', href: '#', } 
+const autoSchool = { name: 'AUTO SCHOOL', href: '#', }
+ 
 function Navigation() {
+  const [hovering, hoveringProps] = useHover()
   return(
     <div className='hidden lg:flex lg:gap-x-6 lg:align-bottom lg:justify-evenly'>
-      {navigation.map((item) => (
-        <a key={item.name} href={item.href} className='text-lg text-bold text-white text-center border-y border-[#000000]/0 hover:border-y hover:border-[#00FF00] focus:outline focus:outline-[#00FF00] duration-100 ease-linear'>
-          {item.name}
+    <div className=''>
+        <a {...hoveringProps} key={service.name} href={service.href} className='text-lg text-bold text-white text-center border-y border-[#000000]/0 hover:border-y hover:border-[#00FF00] focus:outline focus:outline-[#00FF00] duration-100 ease-linear'>
+          {hovering ? service.name : service.name}
         </a>
-      ))}
+    </div>
+    <div className=''>
+        <a key={factAnswerQuestion.name} href={factAnswerQuestion.href} className='text-lg text-bold text-white text-center border-y border-[#000000]/0 hover:border-y hover:border-[#00FF00] focus:outline focus:outline-[#00FF00] duration-100 ease-linear'>
+          {factAnswerQuestion.name}
+        </a>
+    </div>
+    <div className=''>
+        <a key={about.name} href={about.href} className='text-lg text-bold text-white text-center border-y border-[#000000]/0 hover:border-y hover:border-[#00FF00] focus:outline focus:outline-[#00FF00] duration-100 ease-linear'>
+          {about.name}
+        </a>
+    </div>
+    <div className=''>
+        <a key={contact.name} href={contact.href} className='text-lg text-bold text-white text-center border-y border-[#000000]/0 hover:border-y hover:border-[#00FF00] focus:outline focus:outline-[#00FF00] duration-100 ease-linear'>
+          {contact.name}
+        </a>
+    </div>
+    <div className=''>
+        <a key={autoSchool.name} href={autoSchool.href} className='text-lg text-bold text-white text-center border-y border-[#000000]/0 hover:border-y hover:border-[#00FF00] focus:outline focus:outline-[#00FF00] duration-100 ease-linear'>
+          {autoSchool.name}
+        </a>
+    </div>
+
     </div>
   )
 }
