7a351c206229299d91f63715141bd9fa1e2dfc62
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-06-01 20:07:33 -0500

    not much

--- src/components/pageParts/Home/WarrantyAndFinancing.jsx
+++ src/components/pageParts/Home/WarrantyAndFinancing.jsx
@@ -73,10 +73,40 @@ import {
             <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
           </svg>
   
-          <div className="relative max-w-xl lg:max-w-7xl">
+          <div className="relative max-w-7xl">
             <h2 className="pt-10 pb-8 lg:text-7xl lg:max-w-5xl text-stroke text-stroke-fill-[#00ff00] text-stroke-black sm:text-4xl font-Bungee">
               we&apos;ve got you covered,<br />backed by the names you trust
             </h2>
+            <div className='flex flex-1 justify-between pt-4 pb-8'>
+              <a href='#' className='flex basis-1/4 px-3'>
+                <img 
+                  src='/src/assets/images/FactoryMotorPartsPartnersNetwork.png'
+                  alt='Factory Motor Parts Logo'
+                  className=''
+                />
+              </a>
+              <a href='#' className='flex basis-1/4 px-3'>
+                <img 
+                  src='/src/assets/images/SnapLogo.png'
+                  alt='Snap Financing Logo'
+                  className=''
+                />
+              </a>
+              <a href='#' className='flex basis-1/4 px-3'>
+                <img 
+                  src='/src/assets/images/CarFax.png'
+                  alt='CarFax Car Care Service Center Logo'
+                  className=''
+                />
+              </a>
+              <a href='#' className='flex basis-1/4 px-3'>
+                <img 
+                  src='/src/assets/images/BBB.png'
+                  alt='CarFax Car Care Service Center Logo'
+                  className=''
+                />
+              </a>
+            </div>
             <p className="font-Urbanist tracking-wider font-medium text-xl text-white lg:max-w-3xl">
               As proud members of FMP Partners Network Warranty Program, SNAP Financing, CarFax Car Care Service Shop Loyalty Program & Better Business Bureau we have so much to offer!
             </p>
@@ -88,12 +118,14 @@ import {
               <p className="mt-4 text-lg font-Urbanist tracking-wider">
               Don&apos;t be surprised by a breakdown. Factory Motor Parts Network Partners Warranty Program offers one call claims, roadside assistance & many other benefits. After you activate it. Activate your warranty, just go do it. 
               </p>
-              <div className='rounded-full px-3 py-1 mt-4 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20 shadow-xl shadow-black border-b border-[#00ff00] hover:scale-[98%] font-Play'>
-                Been to the shop recently? { ' ' }
-                <a href="#" className="font-semibold text-white">
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  Activate Your Warranty Here
-                </a>
+              <div>
+                <div className='rounded-full px-3 py-1 mt-4 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20 shadow-xl shadow-black border-b border-[#00ff00] hover:scale-[98%] font-Play'>
+                  Been to the shop recently? { ' ' }
+                  <a href="#" className="font-semibold text-white">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                      Activate Your Warranty Here
+                  </a>
+                </div>
               </div>
   
               <dl className="mt-20 space-y-10 font-Urbanist tracking-wide">
@@ -173,14 +205,14 @@ import {
                 Break downs suck when you aren&apos;t prepared, we understand. Snap Financing offers financing for parts & repairs between $300-$5000. Get approved before repairs are made with no credit hit.
                 </p>
                 <div>
-                <p className='rounded-full px-3 py-1 mt-4 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20 shadow-xl shadow-black border-b border-[#00ff00] hover:scale-[98%] font-Play'>
+                  <p className='rounded-full px-3 py-1 mt-4 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20 shadow-xl shadow-black border-b border-[#00ff00] hover:scale-[98%] font-Play'>
                   Interested in Financing?{ '  ' }
+                  
                   <a href="#" className="font-semibold text-white">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Click Here to Apply
-              </a>
+                  <span className="absolute inset-0" aria-hidden="true" />
+                    Click Here to Apply
+                  </a>
                 </p>
-
               </div>
   
                 <dl className="mt-20 space-y-10 font-Urbanist tracking-wide">
