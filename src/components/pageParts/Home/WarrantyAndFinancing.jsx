c0b226e9de2bc3e0c6c48877fa08560cfdb084d9
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-06-04 14:54:48 -0500

    shortening the home page, but saving the removed pieces for use on later pages

--- src/components/pageParts/Home/WarrantyAndFinancing.jsx
+++ src/components/pageParts/Home/WarrantyAndFinancing.jsx
@@ -1,51 +1,3 @@
-import {
-    BoltIcon,
-    CurrencyDollarIcon,
-    GlobeAmericasIcon,
-    CreditCardIcon,
-    ClipboardDocumentListIcon,
-  } from '@heroicons/react/24/outline'
-  
-  const transferFeatures = [
-    {
-      id: 1,
-      name: 'No cost',
-      description:
-        "They say nothing in life is free of cost. Except this warranty. Really. A 12 month / 12,000 mile warranty on parts & the best part it's free",
-      icon: CurrencyDollarIcon,
-    },
-    {
-      id: 2,
-      name: '35,000 participating shops',
-      description:
-        "After your warranty is registered, it's just one call from anywhere in the U.S. & you will be on your way to a repaired car.",
-      icon: GlobeAmericasIcon,
-    },
-    {
-      id: 3,
-      name: 'Roadside Assistance',
-      description:
-        "The one call convenience doesn't end at repairing your vehicle, it also boasts a suite of roadside assistance, Services include towing, lock-out, flat tire changing, fuel, oil, fluid & water deliver service, & jump starts",
-      icon: BoltIcon,
-    },
-  ]
-  const communicationFeatures = [
-    {
-      id: 1,
-      name: 'No Credit Needed',
-      description:
-        "And no hit to your FICO® Score. All you need is a phone number, email address, checking account, steady income of at least $750, and able to enter into a legal contract",
-      icon: CreditCardIcon,
-    },
-    {
-      id: 2,
-      name: 'More Ways to Pay',
-      description:
-        "Each situation is unique which is why your goals depends on how you pay. Choose the lowest overall cost, most flexible, or lowest payment option. Sometimes life gets you downs, but with all these repayment options, it doesn't have too",
-      icon: ClipboardDocumentListIcon,
-    },
-  ]
-  
   export default function WarrantyAndFinancing() {
     return (
       <div className="overflow-hidden bg-[#333333] pb-10 text-white px-20">
@@ -72,7 +24,6 @@ import {
             </defs>
             <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
           </svg>
-  
           <div className="relative max-w-7xl">
             <h2 className="pt-8 pb-4 lg:text-5xl lg:max-w-5xl text-stroke text-stroke-fill-[#00ff00] text-stroke-black sm:text-4xl font-Bungee">
               we&apos;ve got you covered,<br />backed by the names you trust
@@ -111,8 +62,7 @@ import {
               As proud members of FMP Partners Network Warranty Program, SNAP Financing, CarFax Car Care Service Shop Loyalty Program & Better Business Bureau we have so much to offer!
             </p>
           </div>
-  
-          <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
+          <div className="relative mt-12 lg:mt-0 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
             <div className="relative">
               <h3 className="text-2xl font-Urbanist font-semibold tracking-wider text-[#00ff00] sm:text-3xl">12 Month / 12,000 Mile Warranty</h3>
               <p className="mt-4 text-lg font-Urbanist tracking-wider">
@@ -127,57 +77,18 @@ import {
                   </a>
                 </div>
               </div>
-  
-              <dl className="mt-20 space-y-10 font-Urbanist tracking-wide">
-                {transferFeatures.map((item) => (
-                  <div key={item.id} className="relative shadow-xl shadow-[#222222]">
-                    <dt>
-                      <div className="flex h-16 w-16 items-center justify-center hexagon bg-[#00ff00] text-black -ml-8">
-                        <item.icon className="h-12 w-12" aria-hidden="true" />
-                      </div>
-                      <p className="px-10 -mt-8 pt-4 text-xl rounded-t-2xl bg-[#777777] text-[#00ff00] ">{item.name}</p>
-                    </dt>
-                    <dd className="px-10 border-b border-[#00ff00]/50 pb-4 rounded-b-2xl bg-[#777777] text-white shadow-xl shadow-[#222222]">{item.description}</dd>
-                  </div>
-                ))}
-              </dl>
             </div>
-  
-            <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
-              <svg
-                className="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
-                width={784}
-                height={404}
-                fill="none"
-                viewBox="0 0 784 404"
-              >
-                <defs>
-                  <pattern
-                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
-                    x={0}
-                    y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                  </pattern>
-                </defs>
-                <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
-              </svg>
-              <img
-                className="relative mx-auto"
-                width={490}
-                src="/src/assets/images/FactorMotorPartsBothLogos.png"
-                alt=""
+            <div>
+              <img 
+              src="/src/assets/images/Convenience2.jpg"
+              className="hexagon aspect-[6/5]"
               />
             </div>
           </div>
-  
           <svg
-            className="absolute right-full hidden translate-x-1/2 translate-y-12 transform lg:block"
-            width={404}
-            height={784}
+            className="absolute right-full hidden translate-x-1/2 -translate-y-1/4 transform lg:block"
+            width={300}
+            height={700}
             fill="none"
             viewBox="0 0 404 784"
             aria-hidden="true"
@@ -196,8 +107,7 @@ import {
             </defs>
             <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
           </svg>
-  
-          <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div className="relative">
             <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:items-center lg:gap-8">
               <div className="lg:col-start-2">
                 <h3 className="text-2xl font-Urbanist font-semibold tracking-wider text-[#00ff00] sm:text-3xl">Up to $5000 in Financing</h3>
@@ -214,55 +124,59 @@ import {
                   </a>
                 </p>
               </div>
-  
-                <dl className="mt-20 space-y-10 font-Urbanist tracking-wide">
-                  {communicationFeatures.map((item) => (
-                    <div key={item.id} className="relative shadow-xl shadow-[#222222]">
-                      <dt>
-                        <div className="flex h-16 w-16 items-center justify-center hexagon bg-[#00ff00] text-black -ml-8">
-                          <item.icon className="h-12 w-12" aria-hidden="true" />
-                        </div>
-                        <p className="px-10 -mt-8 pt-4 text-xl rounded-t-2xl bg-[#777777] text-[#00ff00] ">{item.name}</p>
-                      </dt>
-                      <dd className="px-10 border-b border-[#00ff00]/50 pb-4 rounded-b-2xl bg-[#777777] text-white shadow-xl shadow-[#222222]">{item.description}</dd>
-                    </div>
-                  ))}
-                </dl>
-              </div>
-  
-              <div className="relative -mx-4 mt-10 lg:col-start-1 lg:mt-0">
-                <svg
-                  className="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
-                  width={784}
-                  height={404}
-                  fill="none"
-                  viewBox="0 0 784 404"
-                  aria-hidden="true"
-                >
-                  <defs>
-                    <pattern
-                      id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
-                      x={0}
-                      y={0}
-                      width={20}
-                      height={20}
-                      patternUnits="userSpaceOnUse"
-                    >
-                      <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                    </pattern>
-                  </defs>
-                  <rect width={784} height={404} fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
-                </svg>
-                <img
-                  className="relative mx-auto"
-                  width={490}
-                  src="/src/assets/images/Snap.png"
-                  alt=""
-                />
-              </div>
             </div>
+            <img 
+              src="/src/assets/images/tools.jpg"
+              className="hexagon aspect-[6/5]"
+              />
           </div>
         </div>
+        <div className="relative mt-12 lg:mt-0 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
+            <div className="relative">
+              <h3 className="text-2xl font-Urbanist font-semibold tracking-wider text-[#00ff00] sm:text-3xl">Maintenance Remainders</h3>
+              <p className="mt-4 text-lg font-Urbanist tracking-wider">
+                As a member of CarFax Car Care Service Shop Loyalty Program, you have the option to receive a monthly vehicle health dashboard, & reminders when services are due via email, after your first visit with us.
+              </p>
+              <div>
+                <div className='rounded-full px-3 py-1 mt-4 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20 shadow-xl shadow-black border-b border-[#00ff00] hover:scale-[98%] font-Play'>
+                  Been to the shop recently? { ' ' }
+                  <a href="#" className="font-semibold text-white">
+                    <span className="inset-0" aria-hidden="true" />
+                      Activate Your Reminders Here
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img 
+              src="/src/assets/images/Quality2.jpg"
+              className="hexagon aspect-[6/5] z-10"
+              />
+            </div>
+            <svg
+            className="absolute left-full hidden -translate-x-1/2 translate-y-1/4 transform lg:block z-0"
+            width={404}
+            height={784}
+            fill="none"
+            viewBox="0 0 404 784"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+          </svg>
+          </div>
       </div>
+    </div>
     )
   }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import { createBrowserRouter, RouterProvider, useHref } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import About from './pages/About.jsx';
 import Services from './pages/Services.jsx';
