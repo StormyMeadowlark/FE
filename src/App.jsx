87c1697f5990b38fc6680e89869b7d6f50557f85
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-06-09 20:44:19 -0500

    started adding consent for cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import NavBar from './components/NavBar.jsx';
 import Footer from './components/pageParts/shared/Footer.jsx';
 import { Outlet } from 'react-router-dom';
+import Consent from './components/pageParts/shared/Consent.jsx';
 const App = () => {
   return (
     <div>
-      <div className='sticky top-0 inset-x-0 z-50 bg-black/50 font-Play'>
-        <NavBar  /> 
+      <div>
+        <div className="sticky top-0 inset-x-0 z-50 bg-black/50 font-Play">
+          <NavBar />
         </div>
-        <div>
-          <Outlet />
-        </div>
-        <div>
-          <Footer />
-        </div>
+        <Consent />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-    )
+  );
   }
 
   export default App;
