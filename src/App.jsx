0c083e75e035d7677ddcb98b954191bcf478b286
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-06-11 15:40:58 -0500

    cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import NavBar from './components/NavBar.jsx';
 import Footer from './components/pageParts/shared/Footer.jsx';
 import { Outlet } from 'react-router-dom';
-import { CookieConsent, Cookies, getCookieConsentValue} from 'react-cookie-consent';
+
 const App = () => {
+
   return (
     <div>
-      <div>
-        <CookieConsent
-          enableDeclineButton
-          location="top"
-          buttonText="yeppers"
-          declineButtonText="noppers"
-          expires={999}
-          overlay
-          debug={true}
-        >
-          Cookies yummy
-        </CookieConsent>
-      </div>
       <div className="sticky top-0 inset-x-0 z-50 bg-black/50 font-Play">
         <NavBar />
       </div>
